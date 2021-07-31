33a62a0acb540f3bd3180484e5ba90d02cdd7e1c
Aparna Viswanath <viswanath.aparna@gmail.com>
2021-07-31 16:41:49 +0530

    Code changes for cleanup

--- app/controllers/music_album_controller.rb
+++ app/controllers/music_album_controller.rb
@@ -3,13 +3,14 @@
 class MusicAlbumController < ApplicationController
   layout "hello_world"
 
-#props for listing all the songs in coillection
+#code for sending props for listing all the songs in coillection
   def index
     @props = { tracks: Track.all}
   end
 
 
-# search functionality code as API
+# search functionality code as API returning json
+# search?popularity=123&album_name=BackstreetBoys&artist_name=Jones
   def search
     @songs = Track.search(params.slice(:popularity, :album_name, :artist_name))
     render json: @songs
